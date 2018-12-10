231c85102d18c98a76e708fb550e2a9915436b48
akageun <akageun@gmail.com>
2018-12-10 18:09:32 +0900

    파일 관리 기능 추가.

--- src/renderer/components/File/File_List.vue
+++ src/renderer/components/File/File_List.vue
@@ -1,45 +1,33 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="w-25">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        Cras justo odio
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </li>
-                    <li class="list-group-item">
-                        Dapibus ac facilisis in
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </li>
-                    <li class="list-group-item">
-                        Morbi leo risus
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </li>
-                    <li class="list-group-item">
-                        Porta ac consectetur ac
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </li>
-                    <li class="list-group-item">
-                        Vestibulum at eros
-                        <span class="badge badge-primary badge-pill">14</span>
+    <div>
+        <div class="row no-gutters">
+            <div class="col-12">
+                <p>
+                    <b>Target Directory</b> : {{targetPath}}
+                </p>
+            </div>
+        </div>
+
+        <div class="row no-gutters">
+            <div class="col-12">
+                <ul id="list-group-ul" class="list-group">
+                    <li class="list-group-item" v-for="file in fileList">
+                        {{file.fileName | liveSubstr}}
+                        <div class="float-right" v-if="file.isDirectory === false">
+                            <span class="badge badge-info badge-pill cursor-pointer" @click="infoFile(file)">INFO</span>
+                            <span class="badge badge-success badge-pill cursor-pointer" @click="copyFile(file.fileName)">COPY</span>
+                            <span class="badge badge-warning badge-pill cursor-pointer" @click="moveFile(file.fileName)">MOVE</span>
+                            <span class="badge badge-danger badge-pill cursor-pointer" @click="deleteFile(file.fileName)">DELETE</span>
+                        </div>
+
+                        <div  v-if="file.clicked">
+                            <hr>
+                            <p>
+                                {{file.statInfo}}
+                            </p>
+                        </div>
                     </li>
                 </ul>
-            </div>
-            <div class="w-75">
-                <div class="row no-gutters">
-                    <div class="col-12">
-                        <p>
-                            {{baseDir}}
-                        </p>
-
-                        <ul id="list-group-ul" class="list-group">
-                            <li class="list-group-item" v-for="file in fileList">
-                                {{file.fileName | liveSubstr}}
-                            </li>
-                        </ul>
-
-                    </div>
-                </div>
             </div>
         </div>
     </div>
@@ -56,7 +44,8 @@
         name: "File_List",
         data() {
             return {
-                baseDir: '',
+                targetPath: '',
+                savePath: '',
                 fileList: {}
             }
         },
@@ -69,17 +58,18 @@
             }
         },
         created() {
-            this.initFileList();
+            const confJson = settingConf.get('file');
+            this.targetPath = confJson.targetPath;
+            this.savePath = confJson.savePath;
 
+            this.initFileList();
 
         },
         methods: {
             initFileList() {
                 let list = [];
-                const confJson = settingConf.get('file');
-                this.baseDir = confJson.targetPath;
 
-                const tmpBaseDir = this.baseDir;
+                const tmpBaseDir = this.targetPath;
                 const readFileList = fs.readdirSync(tmpBaseDir);
 
                 readFileList.forEach(function (file) {
@@ -88,14 +78,106 @@
                     const tmpFileStat = fs.statSync(fullPath);
 
                     let tmpFileJson = {};
+                    tmpFileJson.clicked = false;
                     tmpFileJson.fileName = file;
                     tmpFileJson.isDirectory = tmpFileStat.isDirectory();
-
+                    tmpFileJson.statInfo = tmpFileStat;
 
                     list.push(tmpFileJson);
                 });
 
                 this.fileList = list;
+            },
+            getDateForPath() {
+                const today = new Date();
+                let dd = today.getDate();
+                let mm = today.getMonth() + 1; //January is 0!
+                let yyyy = today.getFullYear();
+                if (dd < 10) {
+                    dd = '0' + dd
+                }
+                if (mm < 10) {
+                    mm = '0' + mm
+                }
+
+                let dt = {};
+                dt['dd'] = String(dd);
+                dt['mm'] = String(mm);
+                dt['yyyy'] = String(yyyy);
+
+                return dt;
+            },
+            mkdirSyncRecursive(directory) {
+                const path2 = directory.replace(/\/$/, '').split(path.sep);
+                console.log(path);
+
+                for (let i = 1; i <= path2.length; i++) {
+                    const segment = path2.slice(0, i).join('/');
+                    !fs.existsSync(segment) ? fs.mkdirSync(segment) : null;
+                }
+            },
+            infoFile(file) {
+                if (file.hasOwnProperty('clicked')) {
+                    file.clicked = !file.clicked;
+                } else {
+                    //since its the first time , set the value pf clicked to true to show the subitem
+                    file = Object.assign({}, file, {clicked: true});
+                }
+                console.log(file);
+
+                const targetFullPath = path.join(this.targetPath, file.fileName);
+
+            },
+            copyFile(fileName) {
+                const dt = this.getDateForPath();
+
+                const tmpSavePath = path.join(this.savePath, dt.yyyy, dt.mm, dt.dd);
+                this.mkdirSyncRecursive(tmpSavePath); //폴더 생성
+
+                const saveFullPath = path.join(tmpSavePath, fileName);
+                if (fs.existsSync(saveFullPath)) {
+                    if (confirm('이미 파일이 존재합니다. 덮어쓰기 하시겠습니까?') == false) {
+                        return;
+                    }
+                }
+
+                const targetFullPath = path.join(this.targetPath, fileName);
+
+                fs.copyFileSync(targetFullPath, saveFullPath);
+                console.log(targetFullPath, ' 는 ', saveFullPath, '로 복사됨');
+
+                this.initFileList();
+            },
+
+            moveFile(fileName) {
+                const dt = this.getDateForPath();
+
+                const tmpSavePath = path.join(this.savePath, dt.yyyy, dt.mm, dt.dd);
+                this.mkdirSyncRecursive(tmpSavePath); //폴더 생성
+
+                const saveFullPath = path.join(tmpSavePath, fileName);
+                if (fs.existsSync(saveFullPath)) {
+                    if (confirm('이미 파일이 존재합니다. 덮어쓰기 하시겠습니까?') == false) {
+                        return;
+                    }
+                }
+
+                const targetFullPath = path.join(this.targetPath, fileName);
+
+                fs.renameSync(targetFullPath, saveFullPath);
+                console.log(targetFullPath, ' 는 ', saveFullPath, '로 이동됨');
+
+                this.initFileList();
+            },
+            deleteFile(fileName) {
+                if (confirm('해당파일을 정말로 삭제하시겠습니까?') == false) {
+                    return;
+                }
+
+                const targetFullPath = path.join(this.targetPath, fileName);
+                fs.unlinkSync(targetFullPath);
+
+                this.initFileList();
             }
         }
     }
--- src/renderer/components/File/index.vue
+++ src/renderer/components/File/index.vue
@@ -1,6 +1,34 @@
 <template>
-    <div>
-        <File_List/>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="w-25">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        Cras justo odio
+                        <span class="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li class="list-group-item">
+                        Dapibus ac facilisis in
+                        <span class="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li class="list-group-item">
+                        Morbi leo risus
+                        <span class="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li class="list-group-item">
+                        Porta ac consectetur ac
+                        <span class="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li class="list-group-item">
+                        Vestibulum at eros
+                        <span class="badge badge-primary badge-pill">14</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-75">
+                <File_List/>
+            </div>
+        </div>
     </div>
 </template>
 
